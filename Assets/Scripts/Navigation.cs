146531b4670fceaa32e4607b0f99a0eae987b49c
MinHeinA <minheinaung97@gmail.com>
2021-11-04 00:50:25 +0800

    Added Navigation Wisp
    - Still broken

--- Assets/Scripts/Navigation.cs
+++ Assets/Scripts/Navigation.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Navigation : MonoBehaviour
 {
+    public GameObject target;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,12 @@ public class Navigation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 diff = target.transform.position - transform.position;
+        diff.Normalize();
+
+        float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rot_z + 90f);
+
+        //transform.LookAt(target.transform);
     }
 }
