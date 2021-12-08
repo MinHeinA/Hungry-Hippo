acb5e126a9f8070c0a43b3e01ad14d97c64a255f
YEOWEIHNGWHYELAB <yeoweihngywh@gmail.com>
2021-12-08 17:16:35 +0800

    popup text

--- Assets/Scripts/TutorialEvent.cs
+++ Assets/Scripts/TutorialEvent.cs
@@ -15,7 +15,7 @@ public class TutorialEvent : MonoBehaviour
         pop.gameObject.SetActive(true);
         Invoke("EmptyBattery", 0.5f);
         //Show WASD Instructions
-        pop.PopUp("W,A,S,D - Move Up, Left, Down and Right");
+        pop.PopUp("W,A,S,D - Move Up, Left, Down and Right, Space - Toggle Flashlight");
     }
     void EmptyBattery()
     {
@@ -29,28 +29,28 @@ public class TutorialEvent : MonoBehaviour
         {
             if (collision.gameObject.name == "Trg_Battery")
             {
-                pop.PopUp("Hello World");
+                pop.PopUp("You have just picked up a battery! Batteries are scattered across the map and is used to replenish your flashlight power.");
                 Destroy(collision.gameObject);
             }
             else if (collision.gameObject.name == "Trg_Mandrake")
             {
-                pop.PopUp(collision.gameObject.name);
+                pop.PopUp("Your objective is to collect all the mandrakes in the map and get to the safe house. Flashlight can be used to stun the hippos if you encounter them.");
                 enemy.SetActive(true);
                 Destroy(collision.gameObject);
             }
             else if (collision.gameObject.name == "Trg_Mandrake_2")
             {
-                pop.PopUp(collision.gameObject.name);
+                pop.PopUp("Once all mandrakes have been collected, RUN! Flashlight will no longer be able to stun the hippo.");
                 Destroy(collision.gameObject);
             }
             else if (collision.gameObject.name == "Trg_Water")
             {
-                pop.PopUp(collision.gameObject.name);
+                pop.PopUp("Water puddles will slow your movement speed, so avoid them if possible!");
                 Destroy(collision.gameObject);
             }
             else if (collision.gameObject.name == "Trg_House")
             {
-                pop.PopUp(collision.gameObject.name);
+                pop.PopUp("Look! There is a safehouse ahead! Get in quick!");
                 Destroy(collision.gameObject);
             }
         }
